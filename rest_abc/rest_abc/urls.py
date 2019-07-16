47a787077d2093f86f641d137144af3c7bc84189
Chengwei Zhang <zhangcw0713@163.com>
2019-07-16 11:38:50 +0800

    update

--- rest_abc/rest_abc/urls.py
+++ rest_abc/rest_abc/urls.py
@@ -1,4 +1,4 @@
-"""quick_start URL Configuration
+"""rest_abc URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.models import User
 
